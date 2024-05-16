8d6f9464a6b946053047fd5488d0b1125df39757
Mayank-Bodas <mayankb2611@gmail.com>
2024-05-16 10:26:40 +0000

    conversion operations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+//console.log(typeof score);
+
+let valurInNumber = Number(score)
+//console.log(typeof valurInNumber);
+//console.log( valurInNumber);  //for the value 33ab the string will convert to NaN not Number , sa,e for the undefined and for null => 0 , 
+
+let isLoggedIn = 1
+// we can convert the 1 and 0 to True and False respectively 
+// when we pass empty strings " " it will give false
+
+
+// *********************************** Operations **************************************
+
+let str1 = "Mayank"
+let str2 = " Bodas"
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2");//32
+
+
+
+
