40ddc3c76a7957c1ca9e59bfc3e459d9c2614a3d
Mayank-Bodas <mayankb2611@gmail.com>
2024-05-16 09:38:59 +0000

    datatypes

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version;
+
+//alert(3) // we are using node js not browser
+
+/* number
+   bigint
+   string=>""
+   boolean = T/F
+   null = stand alone value
+   undefined
+   symbol
+   object
+ */
+
+console.log(typeof null)//object
