d24f844ab0d2ab2d0cb5a1fd4ec2b518b38e8ea4
Mayank-Bodas <mayankb2611@gmail.com>
2024-05-16 11:01:13 +0000

    comparison

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,14 @@
+//>
+// >=
+// <
+// <=
+// ==
+// !=
+
+// you cannot compair two different data types 
+
+// The reason behind == and comparison is that comparison convert the null to 0 that is why null >= 0 is true and null > 0 is false
+
+// === is the strict check it will also check the data type
+
+
