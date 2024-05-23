9cf7fee1e589147bbd0bbbf5531b630ae7df1a24
Mayank-Bodas <mayankb2611@gmail.com>
2024-05-23 08:51:52 +0000

    DOM and Project on DOM

---
+++ 07_Projects_on_Dom/1_Color_Changer/chaiaurcode.js
@@ -0,0 +1,16 @@
+const button = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+button.forEach(function (bt) {
+  bt.addEventListener('click', function (e) {
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
